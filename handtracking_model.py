79d797a08ae17ddd3f0ac68f203e4d469cf3323e
prajwalramagoudapatil <101803815+prajwalramagoudapatil@users.noreply.github.com>
2025-12-09 16:12:05 +0530

    utils for using tensorflow MobileNetV2 ssd lite

---
+++ handtracking_model.py
@@ -0,0 +1,69 @@
+import numpy as np
+# import sys
+import tensorflow as tf
+# import os
+from threading import Thread
+# from datetime import datetime
+import cv2
+# from utils import label_map_util
+from collections import defaultdict
+
+
+PATH_TO_CKPT = 'hand_inference_graph/frozen_inference_graph.pb'
+
+def load_inference_graph():
+    tf.compat.v1.disable_eager_execution()
+
+    # load frozen tensorflow model into memory
+    print("> ====== loading HAND frozen graph into memory")
+    detection_graph = tf.Graph()
+    with detection_graph.as_default():
+        od_graph_def = tf.compat.v1.GraphDef()
+        with tf.io.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+            serialized_graph = fid.read()
+            od_graph_def.ParseFromString(serialized_graph)
+            tf.import_graph_def(od_graph_def, name='')
+        sess = tf.compat.v1.Session(graph=detection_graph)
+    print(">  ====== Hand Inference graph loaded.")
+    return detection_graph, sess
+
+
+if __name__ == '__main__':
+    load_inference_graph()
+
+
+def detect_objects(image_np, detection_graph, sess):
+    # Definite input and output Tensors for detection_graph
+    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+    # Each box represents a part of the image where a particular object was detected.
+    detection_boxes = detection_graph.get_tensor_by_name(
+        'detection_boxes:0')
+    # Each score represent how level of confidence for each of the objects.
+    # Score is shown on the result image, together with the class label.
+    detection_scores = detection_graph.get_tensor_by_name(
+        'detection_scores:0')
+    # detection_classes = detection_graph.get_tensor_by_name(
+    #     'detection_classes:0')
+    # num_detections = detection_graph.get_tensor_by_name(
+    #     'num_detections:0')
+
+    try:
+        image_np = cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)
+    except:
+        print("Could not convert to RGB")
+
+    image_np_expanded = np.expand_dims(image_np, axis=0)
+
+    (boxes, scores) = sess.run(
+        [detection_boxes, detection_scores],
+        feed_dict={image_tensor: image_np_expanded})
+    return np.squeeze(boxes), np.squeeze(scores)
+
+def draw_box_on_image(num_hands_detect, score_thresh, scores, boxes, im_width, im_height, image_np):
+    for i in range(num_hands_detect):
+        if (scores[i] > score_thresh):
+            (left, right, top, bottom) = (boxes[i][1] * im_width, boxes[i][3] * im_width,
+                                          boxes[i][0] * im_height, boxes[i][2] * im_height)
+            p1 = (int(left), int(top))
+            p2 = (int(right), int(bottom))
+            cv2.rectangle(image_np, p1, p2, (77, 255, 9), 3, 1)
